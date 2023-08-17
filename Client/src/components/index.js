e69983565e1fa21e2954ce9d1c0fdd3478ff7437
priyans619 <priyansh.2b@gmail.com>
2023-08-17 14:04:18 +0530

    Export loader

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Navbar } from './Navbar';
 export { default as CustomButton } from './CustomButton';
 export { default as FormField } from './FormField';
 export { default as AddFund } from './AddFund';
+export { default as Loader } from './loader';
